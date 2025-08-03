948df71c58b8d00f8a952c12d11ec4be8b2e5c00
ftmamhmod <ftmamhmod2@gmail.com>
2025-08-03 13:12:25 +0300

    fix: form responsive

--- src/modules/Shared/components/AuthLayout/AuthLayout.jsx
+++ src/modules/Shared/components/AuthLayout/AuthLayout.jsx
@@ -6,7 +6,7 @@ const AuthLayout = () => {
       <div className="auth-container">
         <div className="container-fluid ">
           <div className="row vh-100 justify-content-center align-items-center  ">
-            <div className="col-md-5 rounded-3 bg-white p-5">
+            <div className="col-md-8 col-lg-5 rounded-3 bg-white p-5">
               <div className="logo-container text-center ">
                 <img className="w-50" src={logo} alt="Logo" />
               </div>
